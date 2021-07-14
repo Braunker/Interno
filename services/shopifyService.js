ab0bf9aaaa132a8187870f72326d22cd76fae01f
jaimeokhuysen <jaimeob@braunker.com>
2021-07-14 13:25:12 -0500

    Update shopifyService.js

--- services/shopifyService.js
+++ services/shopifyService.js
@@ -17,13 +17,13 @@ module.exports = {
       +'.myshopify.com/admin/api/'+process.env.SHOPIFY_API_VERSION+'/inventory_levels/set.json';
 
       throttle(function(){
-        console.log('attempting to post inventory level');
         request.post({url:inventoryLevelUrl,form:{'location_id': process.env.SHOPIFY_LOCATION_ID, 'inventory_item_id': element.inventory_item_id,
         'available':element.available}}, function (err, res, body) {
       		if(err){
             console.log(err);
             reject(err);
           }
+          console.log(res);
           resolve(res);
       	});
       });
@@ -34,12 +34,12 @@ module.exports = {
       var inventoryItemUrl = 'https://'+process.env.SHOPIFY_API_KEY+':'+process.env.SHOPIFY_PASSWORD+'@'+process.env.SHOPIFY_SHOPNAME
       +'.myshopify.com/admin/api/'+process.env.SHOPIFY_API_VERSION+'/variants/'+element.variant_id+'.json';
       throttle(function(){
-        console.log('attempting to post price');
         request.put({url:inventoryItemUrl,form:{"variant":{"id":element.variant_id,"price":element.price}}}, function (err, res, body) {
       		if(err){
             console.log(err);
             reject(err);
           }
+          console.log(res);
           resolve(res);
       	});
       });
